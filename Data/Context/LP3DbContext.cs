eee638f9d89e0ff90dde1e7f455fdba67e017d8d
Felix Benzan <felixbenz07@gmail.com>
2022-11-04 19:48:21 -0400

    Vehiculosa dd

--- Data/Context/LP3DbContext.cs
+++ Data/Context/LP3DbContext.cs
@@ -10,6 +10,9 @@ public class LP3DbContext:DbContext,ILP3DbContext
         
     }
     public virtual DbSet<Producto> Productos {get; set;} = null!;
+    public virtual DbSet<VehiculoMarca> Marcas {get; set;} = null!;
+    public virtual DbSet<VehiculoModelo> Modelos {get; set;} = null!;
+    public virtual DbSet<Vehiculo> Vehiculos {get; set;} = null!;
     public override int SaveChanges()
     {
         return base.SaveChanges();
@@ -23,6 +26,10 @@ public class LP3DbContext:DbContext,ILP3DbContext
 public interface ILP3DbContext
 {
     public DbSet<Producto> Productos {get; set;}
+    public  DbSet<VehiculoMarca> Marcas {get; set;}
+    public  DbSet<VehiculoModelo> Modelos {get; set;}
+    public  DbSet<Vehiculo> Vehiculos {get; set;}
+
     public int SaveChanges();
     public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 }
---
+++ Data/Models/Vehiculo.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace  LP3.Data.Models;
+
+public class Vehiculo
+{
+    public int Id { get; set; } 
+    public VehiculoModelo Modelo { get; set; }
+    public int Anio { get; set; }
+}
+
+public class VehiculoMarca
+{
+    public int Id { get; set; } 
+    public string Nombre { get; set; }
+}
+
+public class VehiculoModelo
+{
+    public int Id { get; set; } 
+    public VehiculoMarca Marca { get; set; }
+    public string Nombre { get; set; }
+}
